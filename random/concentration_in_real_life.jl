3b4bef4ef8ccc85d9da21a7973ef1609d2b4c892
Thopag <100088550+Thopag@users.noreply.github.com>
2026-03-18 15:57:55 +0100

    add extra lidocaine concentraion

--- random/concentration_in_real_life.jl
+++ random/concentration_in_real_life.jl
@@ -1,5 +1,6 @@
 
-M = 234.337 # g/mol
+#M = 234.337 # g/mol
+M = 270.8 # g/mol
 
 # Safety and Tolerability of the Lidocaine Patch 5%, a Targeted Peripheral Analgesic: A Review of the Literature
 C = 150                 # ng/ml
@@ -7,8 +8,9 @@ C = C*(10^-9)/(10^-3)   # g/l
 C = C/M                 # M
 C = C*10^6              # µM
 println("Safety and Tolerability of the Lidocaine Patch 5%, a Targeted Peripheral Analgesic: A Review of the Literature")
+println("IN PLASMA")
 println("$(round(C, digits=2)) µM")
-println()
+println("----------------------------------------------------------------------")
 
 # Computer-controlled lidocaine infusion for the evaluation of neuropathic pain after peripheral nerve injury
 C = 2                   # µg/ml
@@ -16,14 +18,30 @@ C = C*(10^-6)/(10^-3)   # g/l
 C = C/M                 # M
 C = C*10^6              # µM
 println("Computer-controlled lidocaine infusion for the evaluation of neuropathic pain after peripheral nerve injury")
+println("IN PLASMA")
 println("$(round(C, digits=2)) µM")
-println()
+println("----------------------------------------------------------------------")
 
 # Determination of the Minimum Local Analgesic Concentrations of Epidural Bupivacaine and Lidocaine in Labor 
 C = 0.4                 # %w/v
-C = C/100               # g/ml
+C = 10*C                # mg/ml
+C = C/(10^3)            # g/ml
 C = C/10^-3             # g/l
 C = C/M                 # M
 C = C*10^6              # µM
 println("Determination of the Minimum Local Analgesic Concentrations of Epidural Bupivacaine and Lidocaine in Labor")
+println("IN DRUG")
 println("$(round(C, digits=2)) µM")
+println("----------------------------------------------------------------------")
+
+# Lidocaine: A Local Anesthetic, Its Adverse Effects and Management
+C = 2                   # % (I suppose) <=> %w/v 
+C = 10*C                # mg/ml
+C = C/(10^3)            # g/ml
+C = C/(10^-3)           # g/l
+C = C/M                 # M
+C = C*10^6              # µM
+println("Lidocaine: A Local Anesthetic, Its Adverse Effects and Management")
+println("IN DRUG")
+println("$(round(C, digits=2)) µM")
+println("----------------------------------------------------------------------")
