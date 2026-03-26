00f05464d0860eb1e19948af5f577ecf457e21d4
Thopag <thomas.pagano@outlook.fr>
2026-03-26 17:06:36 +0100

    typo

--- parameter_analyses.jl
+++ parameter_analyses.jl
@@ -1,7 +1,7 @@
 include("DIV0/params.jl")
 include("DIV7/params.jl")
 
-folder = "DIV7"
+folder = "DIV0"
 
 if folder == "DIV0"
     get_param = DIV0_parameter
@@ -99,7 +99,7 @@ function main()
     # amps = [55.0]#0:25:150.0
     # labels = ["$amp pA" for amp in amps]
 
-    lido_concentrations = [0.0, 1.0, 10.0, 50.0, 100.0, 500.0, 1000.0]
+    lido_concentrations = [0.0]#, 1.0, 10.0, 50.0, 100.0, 500.0, 1000.0]
     labels = ["$C_lido" for C_lido in lido_concentrations]
 
     # original_vec = [true, false]
