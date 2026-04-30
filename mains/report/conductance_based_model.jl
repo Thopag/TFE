f8a48c5bd71335c517512d6123a299282a325340
Thopag <100088550+Thopag@users.noreply.github.com>
2026-04-30 16:13:38 +0200

    ..

--- mains/report/conductance_based_model.jl
+++ mains/report/conductance_based_model.jl
@@ -7,17 +7,17 @@ function steady_state()
     inactivation(V) = 1 / (1 + exp((V+65)/6) )
 
     xticks = []
-    plt = plot(xlabel="Voltage (mV)", ylabel= "(-)", legend=:right, xticks = xticks, legendfontsize=10)
+    plt = plot(xlabel="Voltage (mV)", ylabel= L"p_{inact,\infty} (-)", legend=:right, xticks = xticks, legendfontsize=10)
 
     channel_availability = (activation.(V).^3) .* inactivation.(V)
     channel_availability = channel_availability ./ maximum(channel_availability)
 
-    plot!(plt, V, activation.(V), label="", c=:black, linestyle=:solid, linewidth = 2)
-    #plot!(plt, V, inactivation.(V), label="", c=:black, linestyle=:dash, linewidth = 2)
+    #plot!(plt, V, activation.(V), label="", c=:black, linestyle=:solid, linewidth = 2)
+    plot!(plt, V, inactivation.(V), label="", c=:black, linestyle=:dash, linewidth = 2)
     #plot!(plt, V, channel_availability, label="normalized channel availability", c=:black, linestyle=:dot, linewidth = 2)
 
     display(plt)
-    savefig(plt, "plots/report/activation.pdf")
+    savefig(plt, "plots/report/inactivation.pdf")
 end
 
 steady_state()
