0259f861a67b5a2c13473ceed0e337f9ef15dd2a
Thopag <100088550+Thopag@users.noreply.github.com>
2026-06-21 18:55:14 +0200

    used, typo

--- TFE_package/src/system/with_synapse/gate_variable/NMDA.jl
+++ TFE_package/src/system/with_synapse/gate_variable/NMDA.jl
@@ -16,7 +16,6 @@ function dot_P_NMDA(P_NMDA, tau_rec_NMDA)
     return (1-P_NMDA)/tau_rec_NMDA
 end
 
-# Not used
 function NMDA_Mg_block(V) 
     mgo = 1.0               #[mM]
     m = 1 / (1 + exp(0.062 * -V) * (mgo / 3.57 ))
